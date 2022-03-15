114a77c2087413aae62a397186a9a29c8d5119d4
priorityc <petya83dimitrova@gmail.com>
2022-03-15 12:08:01 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+//project 11
+let myname = 'Petya';
+let myage = 38;
+console.log('hello my name is'+ myname + 'and I am'+myage + 'years old');
+let my= 'John';
+let surname ='Doe'
+console.log (my+''+surname);
+
+//Project12 calcolate your dog age
+let firstDogyear = 15;
+let secondDogyear = 9;
+let myDogage = (firstDogyear+secondDogyear)+36*4;
+console.log(myDogage);
+
+//project13 set arey call the items,add items to the arrey
+let Arrey = ['breackfast','lunch','diner'];
+console.log(Arrey[0]);
+console.log(Arrey[1]);
+console.log(Arrey[2]);
+Arrey.push('snack');  //add push
+Arrey.push('afternoonsnack');
+console.log(Arrey);
+let index=Arrey.indexOf('healthy');//update an item in the arrey
+Arrey [0]=('healthy breackfast');
+console.log(Arrey);
+//task2 
+let number1=prompt('Please enter one number');//imput two numbers
+let number2=prompt('please enter second number');
+let result=number1*number2;//multyply the results
+
+let result1=alert('Your result is '+ result);//prompt whit multipyed results
+//task3
+
+let value=prompt('What is your favorite number?');
+let result2=value;
+if (value='');
+  alert('You number is' + result2);//input one value and compare it through Bolean logic to return the result
+
+ 
+ //prompt whit inputs
+
+ print=(e)=>{
+  e.preventDefault();
+ }
+
+  
+  let input=prompt('what is your favorite colour?');
+  let result3=input;
+if (input='') {
+  document.querySelector('#print').innerHTML;
+  document.querySelector('#print').addEventListener= ('click',result3);
+}
+   
